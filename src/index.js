330cd39b315e91f472f86f81794399edfc46c56f
louie_gomez <kinggomez027.lg@gmail.com>
2022-09-02 18:35:02 -0400

    Additional Routes

--- src/index.js
+++ src/index.js
@@ -11,12 +11,45 @@ function Home() {
   );
 }
 
+function About() {
+  return (
+    <div>
+      <h1>This is my about component!</h1>
+    </div>
+  );
+}
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="Username" />
+        </div>
+        <div>
+          <input type="text" name="password" placeholder="Password" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+    </div>
+  );
+}
+
 // Step 2. Use <Route> components to define client-side routes in our app
 function App() {
   return (
-    <Route path="/">
-      <Home />
-    </Route>
+    <div>
+      <Route path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+    </div>
   );
 }
 
